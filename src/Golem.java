a3704008b9c0a1cf3b0a88cede957978bccbcaa8
E343C <e.cesari@studenti.unibs.it>
2021-05-02 12:26:16 +0200

    0.1

--- src/Golem.java
+++ src/Golem.java
@@ -1,4 +1,36 @@
 public class Golem
 {
+	public final static int VITA_MAX = 10;
 
+	private String nome;
+	private int vita;
+	//private ArrayList<Elementi> stones = new ArrayList<>();
+	private int selectedStone = 0; // indice della pietra selezionata
+
+	public Golem(String nome /*,arraylist stones*/) {
+		this.nome = nome;
+		this.vita = VITA_MAX;
+		//this.stones=stones;
+	}
+
+	public Golem(/*,arraylist stones*/) {
+		this.vita = VITA_MAX;
+		//this.stones=stones;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getVita() {
+		return vita;
+	}
+
+	public void setVita(int vita) {
+		this.vita = vita;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
